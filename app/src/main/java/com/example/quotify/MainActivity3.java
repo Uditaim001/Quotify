9e7853c899e9ca550f209fdfb3d67a19c4937fb0
Udit <udit.shukla@gonuclei.com>
2024-08-13 18:22:40 +0530

    Multiple Quotes

--- app/src/main/java/com/example/quotify/MainActivity3.java
+++ app/src/main/java/com/example/quotify/MainActivity3.java
@@ -1,6 +1,10 @@
 package com.example.quotify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public class MainActivity3 extends AppCompatActivity {
 
+    Button generate;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +37,22 @@ public class MainActivity3 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        generate=findViewById(R.id.button3);
+        editText=findViewById(R.id.editTextNumber);
+//        if(editText.length()>0){
+//
+//        }
+//        int num= Integer.parseInt(editText.getText().toString());
+        generate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity3.this, MainActivity4.class);
+
+                intent.putExtra("num",10);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/quotify/MainActivity4.java
+++ app/src/main/java/com/example/quotify/MainActivity4.java
@@ -1,6 +1,10 @@
 package com.example.quotify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity4 extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public class MainActivity4 extends AppCompatActivity {
+    interface MultipleQuotes{
+        @GET("/quotes/random?limit={num}")
+        Call<List<model>> getmdata(@Path("num") int num);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +37,47 @@ public class MainActivity4 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        Intent intent = getIntent();
+        int num = intent.getIntExtra("num", 2);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.quotable.io")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MultipleQuotes multipleQuotes = retrofit.create(MultipleQuotes.class);
+        multipleQuotes.getmdata(num).enqueue(new Callback<List<model>>() {
+            @Override
+            public void onResponse(Call<List<model>> call, Response<List<model>> response) {
+                List<model> data = response.body();
+                List<String> quotesList = new ArrayList<>();
+
+                for (int i = 0; i < num; i++) {
+                    String quote = data.get(i).content;
+                    String author = data.get(i).author;
+                    quotesList.add(quote + "\n— " + author);
+                }
+
+                ListView listView = findViewById(R.id.listview);
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                        MainActivity4.this,
+                        android.R.layout.simple_list_item_1,
+                        quotesList
+                );
+
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<model>> call, Throwable throwable) {
+                // Handle failure
+                Toast.makeText(MainActivity4.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
